5320325824db473133177876bd4cacf80e8a2d90
thomas.fattal <thomas.fattal@877c904e-b753-11de-b71e-ab0a53e69959>
2009-11-24 20:26:38 +0000

    Added Algo_Kruskal

---
+++ trunk/TSP/src/CreationGraphe.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+
+public class CreationGraphe
+{
+	protected Graphe g;
+	
+	public CreationGraphe (Graphe g)
+	{
+		this.g = g;
+	}
+	
+	public ArrayList<Point> copieListe (ArrayList<Point> liste)
+	// Méthode permettant de copier une liste passée en paramètre.
+	// Renvoie une liste.
+	{
+		ArrayList <Point> copie = new ArrayList<Point>();
+		for (Point p : liste)
+		{
+			copie.add(p);
+		}
+		return copie;
+	}
+	
+	public void afficheliste (ArrayList<Point> liste)
+	// Affichage d'une liste.
+	{
+		for (Point p : liste)
+		{
+			System.out.println(p.getIdent() + " - " + p.getX() + ", " + p.getY());
+		}
+	}
+	
+	public void afficheAretes (ArrayList<Aretes> liste)
+	{
+		for (Aretes a : liste)
+		{
+			a.afficheAretes();
+		}
+	}
+	
+}
